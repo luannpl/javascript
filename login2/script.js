af04eb8a0fee0836c1dfee1ae9f4ce907e87748c
Paulo Luan <pauloluan1409@gmail.com>
2024-07-05 22:16:59 -0300

    Atualizando diretório

--- login2/script.js
+++ login2/script.js
@@ -9,12 +9,12 @@ let emailLogin = prompt("Digite seu email: ");
     for(let i = 0 ; i<emails.length;i++){
         if(emailLogin == emails[i]){
             indexEmail = i;
-            console.log(indexEmail);
-            break
+            // console.log(indexEmail);
+            break;
         }
     }
     if(indexEmail == undefined){
-        console.log("email nao encontrado")
+        console.log("email nao encontrado");
     } else {
         for(let x = 0; x<senhas.length;x++){
             let senhaLogin = prompt("Digite sua senha: ");
@@ -34,60 +34,3 @@ let emailLogin = prompt("Digite seu email: ");
     }   
 }
 
-
-
-
-// let emailLogin;
-
-// function email(){
-//      emailLogin = prompt("Digite seu email: ");
-//     for(let i = 0; i<emails.length;i++){
-//         if(emailLogin == emails[i]){
-//             alert("Email correto")
-//             break;
-//         }
-//         else{
-//             continue
-    
-//     }}
-// }
-
-// function senha(){
-//     let senhaLogin = prompt("Digite sua senha: ");
-//     for(let i = 0; i<senhas.length;i++){
-//         if(senhaLogin == senhas[i]){
-//             alert("Senha correta")
-//             break;
-//         }
-//         else{
-//             continue
-    
-//     }}
-// }
-
-
-
-// email();
-// senha();
-
-
-// function senha(){
-//     for(let i = 0; i<senhas.length; i++){
-//         if(senhaLogin == senhas[i]){
-//             console.log("Senha encontrada")
-//         }
-//     }
-// }
-// let emailLogin = prompt("Digite seu email: ");
-// let senhaLogin = prompt("Digite sua senha: ");
-
-
-
-// while()
-// for(let i = 0;i<emails.length; i++){
-//     while(emailLogin != emails[i] && senhaLogin != senhas[i]){
-//         emailLogin = prompt("Digite seu email: "); 
-//         senhaLogin = prompt("Digite sua senha: ");
-//     }
-
-// }
