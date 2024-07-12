d02cfebf4515da090aaf5f7090f4d5394280b305
Paulo Luan <pauloluan1409@gmail.com>
2024-07-12 09:49:58 -0300

    Event

--- objeto/estados/script.js
+++ objeto/estados/script.js
@@ -1,36 +1,40 @@
-import ufs from './estados.js'
+import ufs from "./estados.js";
 
 let tagUl = document.getElementById("lista-estados");
 
-// for(let i = 0; i<ufs.length;i++){
-    
-//     tagUl.innerHTML += `<li>Estado: ${ufs[i].nome}, Região: ${ufs[i].regiao.nome}`
-// }
-let chave =0;
-let interval = setInterval(function(){
-    tagUl.innerHTML += `<li>Estado: ${ufs[chave].nome} <br> Região: ${ufs[chave].regiao.nome}</li><br>`
-    chave++;
-    console.log(chave)
-},200)
+tagUl.addEventListener("dblclick", function () {
+  alert("Clicou");
+});
 
-let stop = ufs.length;
-setTimeout(function(){
-        clearInterval(interval)
-},stop*200)
+for (let i = 0; i < ufs.length; i++) {
+  tagUl.innerHTML += `<li id="lis${i}">Estado: ${ufs[i].nome},<br> Região: ${ufs[i].regiao.nome}</li><br>`;
+}
 
+for (let i = 0; i < ufs.length; i++) {
+  let tagLi = document.getElementById(`lis${i}`);
+  tagLi.addEventListener("click", function () {
+    alert(`Estado: ${ufs[i].nome}, Região: ${ufs[i].regiao.nome}`);
+  });
+}
 
+// let chave =0;
+// let interval = setInterval(function(){
+//     tagUl.innerHTML += `<li>Estado: ${ufs[chave].nome} <br> Região: ${ufs[chave].regiao.nome}</li><br>`
+//     chave++;
+//     console.log(chave)
+// },200)
+
+// let stop = ufs.length;
+// setTimeout(function(){
+//         clearInterval(interval)
+// },stop*200)
 
 // let interval = setInterval(function(){
 //     tagUl.innerHTML += "<li>Conteúdo de texto</li>"
 // },2000);
 
-
 // setTimeout(function(){
 //     clearInterval(interval);
 // },10000)
 
-
-
 // console.log(tagUl)
-
-
